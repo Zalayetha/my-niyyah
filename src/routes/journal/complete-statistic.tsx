c452f9f10832e7f58636a0801c794f8148eb3d9a
zalayetha <zalayetha@gmail.com>
2026-06-08 08:26:25 +0700

    style: add info text

--- src/routes/journal/complete-statistic.tsx
+++ src/routes/journal/complete-statistic.tsx
@@ -30,6 +30,7 @@ function RouteComponent() {
       <div className="text-3xl text-white font-medium text-center font-sans">
         Statistik Lengkap
       </div>
+
       <div className="grid grid-cols-8 grid-rows-6 mt-8 mx-8  font-sans gap-x-6 gap-y-4">
         {/*Row 1 : Day Name*/}
         <div className="text-white font-medium text-xs"></div>
@@ -106,8 +107,12 @@ function RouteComponent() {
         <div className="h-8 w-8 bg-cyan-900 rounded-xl"></div>
         <div className="h-8 w-8 bg-lime-200 rounded-xl"></div>
       </div>
+      <div className="text-md text-white font-normal text-center font-sans mt-8">
+        Kualitas perasaanmu ketika solat
+      </div>
+
       <DonutChart
-        className="mt-8 mx-2"
+        className="mt-4 mx-2"
         data={weeklyPrayerData}
         size={200}
         strokeWidth={24}
